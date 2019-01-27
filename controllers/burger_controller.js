d6bc944cae84eddfbbea0aa66fa986e57c3e982d
Speedway <harpalassi@gmail.com>
2019-01-27 16:16:33 -0600

    mysql data appearing on frontend

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -7,11 +7,11 @@ const burger = require("../models/burger.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
-      var hbsObject = {
+    
+      console.log(data);
+      res.render("index", {
         burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+      });
     });
   });
 
