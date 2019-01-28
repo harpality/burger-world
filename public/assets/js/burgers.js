8c880db432db2a3bf273e8a50358b521ebfac75b
Speedway <harpalassi@gmail.com>
2019-01-28 12:58:38 -0600

    added favicon

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,44 +1,40 @@
 $(function() {
-    $(".devour").on("click", function(event) {
-      let id = $(this).data("id");
-        console.log(id)
-      var devoured = {
-        devoured: true
-      };
-  
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: devoured
-      }).then(
-        function() {
-          console.log("burger devoured");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+  $(".devour").on("click", function(event) {
+    let id = $(this).data("id");
+    console.log(id);
+    var devoured = {
+      devoured: true
+    };
 
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-            
-     let newBurger = {
-        burger_name: $("#burger").val().trim()
-     };
-    
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devoured
+    }).then(function() {
+      console.log("burger devoured");
+      // Reload the page to get the updated list
+      location.reload();
     });
-})
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    let newBurger = {
+      burger_name: $("#burger")
+        .val()
+        .trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function() {
+      console.log("created new burger");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+});
